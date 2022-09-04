f1ae2077b3dc68a8387dddff48aa828648c7e087
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-09-04 02:25:43 +0000

    Create nav component

---
+++ src/components/Navbar/Nav.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import NavLink from './NavLink'
+
+const Nav = () => {
+  const navRef = useRef()
+  const sectionTitles = ['portfolio', 'skills', 'contact']
+
+  useEffect(() => {
+    gsap.fromTo(
+      navRef.current,
+      { x: 30, opacity: 0 },
+      { x: 0, opacity: 1, delay: 0.3, dureation: 0.5 }
+    )
+  }, [])
+
+  return (
+    <nav className='nav' ref={navRef}>
+      {sectionTitles.map((sectionTitle) => (
+        <NavLink sectionTitle={sectionTitle} />
+      ))}
+    </nav>
+  )
+}
+
+export default Nav
