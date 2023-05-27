5960b4a39a341cda712836cdeabc49f12c990bfe
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2023-05-27 18:01:51 -0400

    Modify base style references

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import Header from './components/Header'
-import Home from './pages/Home'
-import Footer from './components/Footer'
-import './App.css'
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
 
-      <main className='main'>
+      <main>
         <Home />
       </main>
 
