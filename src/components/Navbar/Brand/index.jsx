7e80fa6dbde32f0bde1c8d38fb8e46b3fdc42ab7
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2023-05-28 21:13:42 -0400

    Remove gsap

--- src/components/Navbar/Brand/index.jsx
+++ src/components/Navbar/Brand/index.jsx
@@ -1,21 +1,8 @@
-import React, { useEffect, useRef } from "react"
-import { gsap } from "gsap"
-
 import classes from "./brand.module.css"
 
 const Brand = () => {
-  const brandRef = useRef()
-
-  useEffect(() => {
-    gsap.fromTo(
-      brandRef.current,
-      { x: -30, opacity: 0 },
-      { x: 0, opacity: 1, delay: 0.3, duration: 0.5 }
-    )
-  }, [])
-
   return (
-    <div className={classes.brand} ref={brandRef}>
+    <div className={classes.brand}>
       <a href="#about">Darrick Fauvel</a>
     </div>
   )
--- src/components/Navbar/Nav/index.jsx
+++ src/components/Navbar/Nav/index.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect, useRef } from "react"
-import { gsap } from "gsap"
-
 import classes from "./nav.module.css"
 import NavLink from "../../shared/NavLink"
 
 import sectionTitles from "../../shared/sectionTitles"
 
 const Nav = () => {
-  const navRef = useRef()
-
-  useEffect(() => {
-    gsap.fromTo(
-      navRef.current,
-      { x: 30, opacity: 0 },
-      { x: 0, opacity: 1, delay: 0.3, dureation: 0.5 }
-    )
-  }, [])
-
   return (
-    <nav className={classes.nav} ref={navRef}>
+    <nav className={classes.nav}>
       {sectionTitles.map((sectionTitle) => (
         <NavLink sectionTitle={sectionTitle} key={sectionTitle} />
       ))}
